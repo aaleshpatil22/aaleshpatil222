54742d97b835650d915bf4c8cabe1b2d591a58a3
aaleshpatil22 <102527704+aaleshpatil22@users.noreply.github.com>
2023-01-14 18:03:31 +0530

    Add files via upload

---
+++ Map and Reduce/must.java
@@ -0,0 +1,12 @@
+import java.util.StringTokenizer;
+class Must{
+
+   public static void main (String a[]){
+   
+   String s = "पुणे ";   
+   System.out.println(s.startsWith("प"));
+   System.out.println(s.length());
+   System.out.println(s.charAt(2));
+   
+   }
+}
